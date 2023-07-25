aa610801deb44ba2a1b12089e0cf404927a7430c
Nawaf v p <nawafvp.dev@gmail.com>
2023-07-25 19:41:06 +0530

    signup created

--- client/let-them-cook/src/components/Register.jsx
+++ client/let-them-cook/src/components/Register.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [_, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      alert("Registration Completed! Now login.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
-}
+    <div className="auth-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
 
 export default Register
