7ea5ec7da65740e72d2ae50428b17cc07821852f
Nawaf v p <nawafvp.dev@gmail.com>
2023-08-15 23:47:13 +0530

    user registration issue fixed

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -6,22 +6,28 @@ const router = express.Router();
 import { UserModel } from "../models/Users.js";
 
 
-// ... (other imports and code)
-
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
+  if (!password) {
+    return res.status(400).json({ message: "Password is missing" });
+  }
+
+  //console.log("Password:", password);
   const user = await UserModel.findOne({ username });
 
   if (user) {
     return res.json({ message: "User already exists" });
   }
 
+
   try {
     const salt = await bcrypt.genSalt(10);
-    console.log("Salt:", salt); // Debug: Check the salt value
+    //console.log("Salt:", salt); 
+    //console.log("Password:", password);
     const hashedPassword = await bcrypt.hash(password, salt);
-    console.log("Hashed Password:", hashedPassword); // Debug: Check the hashed password
+    
+    //console.log("Hashed Password:", hashedPassword); 
     const newUser = new UserModel({ username, password: hashedPassword });
     await newUser.save();
     res.json({ message: "User registered successfully" });
@@ -30,6 +36,7 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ message: "Error registering user" });
   }
 });
+
 
 
 //const salt = await bcrypt.genSalt(10);
@@ -61,7 +68,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
-  
+    
     const user = await UserModel.findOne({ username });
   
     if (!user) {
@@ -75,7 +82,7 @@ router.post("/login", async (req, res) => {
         .status(400)
         .json({ message: "Username or password is incorrect" });
     }
-    const token = jwt.sign({ id: user._id }, "secret");
+    const token = jwt.sign({ id: user._id }, "secret"); 
     res.json({ token, userID: user._id });
   });
   
