7aae1a40f42283968172e8283b99c415a13a10e8
Nawaf v p <nawafvp.dev@gmail.com>
2023-09-06 20:54:07 +0530

    removed unwanted code

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -38,34 +38,6 @@ router.post("/register", async (req, res) => {
 });
 
 
-
-//const salt = await bcrypt.genSalt(10);
-/* router.post("/register", async (req, res) => { //req - getting data from . res - used to send data to back to whoever made that API request
-
-    const {username, password}= req.body;//defining endpoints in API
-    
-    const user = await UserModel.findOne({
-                                       username: username
-                                       });
-
-    if(user){
-        return res.json({message:"user already exists"});
-    }           
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      const newUser = new UserModel({ username, password: hashedPassword });
-      await newUser.save();
-      res.json({ message: "User registered successfully" });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ message: "Error registering user" });
-    }
-    
-    
-      
-}); */
-
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
     
