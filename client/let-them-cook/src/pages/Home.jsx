11bbbe475163000c99604403bb5a462927698a7b
Nawaf v p <nawafvp.dev@gmail.com>
2023-09-16 21:06:23 +0530

    removed unwanted code

--- client/let-them-cook/src/pages/Home.jsx
+++ client/let-them-cook/src/pages/Home.jsx
@@ -233,7 +233,7 @@ export const Home = () => {
                     xmlns="http://www.w3.org/2000/svg" 
                     viewBox="0 0 24 24" 
                     fill="currentColor" 
-                    className="w-6 h-6"
+                    className="w-6 h-6 fill-rose-600"
                     >
                     <path 
                         d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
@@ -356,44 +356,6 @@ export const Home = () => {
 
 export default Home;
 
-
-{/* <header>
-<h2 class="text-gray-700 text-6xl font-semibold">Recipes</h2>
-<h3 class="text-2xl font-semibold">For Ninjas</h3>
-</header>
-<div class="">
-<h4 class="font-bold mt-12 pb-2 border-b border-gray-200">
-  Latest Recipes
-</h4>
-<div class="mt-8 grid lg:grid-cols-3 gap-10">
-  <div class="card hover:shadow-lg">
-    <img
-      src="https://i.ibb.co/tpCdNcX/stew.jpg"
-      alt="stew"
-      class="w-full h-32 sm:h-48 object-cover"
-    />
-    <div class="m-4">
-      <span class="font-bold">5 Bean Chilli Stew</span>
-      <span class="block text-gray-500 text-sm">Recipe by Mario</span>
-    </div>
-    <div class="badge">
-      <svg
-        class="w-5 inline-block"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <span>25 mins</span>
-    </div>
-  </div> */}
 
 
 
