3039efaf072d31249a994b788bbccfc5f35b6db9
Nawaf v p <nawafvp.dev@gmail.com>
2023-09-16 22:01:58 +0530

    home UI updated

--- client/let-them-cook/src/pages/Home.jsx
+++ client/let-them-cook/src/pages/Home.jsx
@@ -199,8 +199,8 @@ export const Home = () => {
               <li key={recipe._id}>
                 <div className="card rounded-xl shadow-xl py-4 px-2 hover:scale-105 hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
                 <img
-                  src="https://i.ibb.co/tpCdNcX/stew.jpg"
-                  alt="stew"
+                  src={recipe.imageUrl}
+                  alt={recipe.name} 
                   className="w-full h-32 sm:h-48 rounded-md object-cover"
                 />
                 <div className="m-4">
